3531b15198c542b2e5dac680c8140b52f051c72c
Armaritto <armia.joseph35@gmail.com>
2025-07-22 20:26:11 +0300

    work done: register the db context in Program.cs
    Co-Authored-By: Mohamed Khaled <59765067+MohammedKhaliid@users.noreply.github.com>
    Co-Authored-By: Ayman Osama Algamal <52029584+ayman-art@users.noreply.github.com>

---
+++ Data/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skill_Hub.Models;
+
+namespace Skill_Hub.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
