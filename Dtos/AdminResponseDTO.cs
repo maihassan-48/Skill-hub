8088b4fc4571e12460f61a3e412c60ebfa935b2d
Mohamed Khaled <mohamed.khaled1872002@gmail.com>
2025-07-23 11:29:39 +0300

    work done:
    add admin, student, instructor DTOs

---
+++ Dtos/AdminResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using Skill_Hub.Enums;
+
+namespace SkillHub.DTOs
+{
+    public class AdminResponseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Role Role { get; set; } = Role.Admin;
+        public string? Permissions { get; set; }
+
+    }
+}
---
+++ Dtos/StudentResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using Skill_Hub.Enums;
+
+namespace SkillHub.DTOs
+{
+    public class StudentResponseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Role Role { get; set; } = Role.Student;
+        public DateTime DateOfBirth { get; set; }
+        public string Major { get; set; }
+    }
+}
