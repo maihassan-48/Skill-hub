67a649161a9917a2c04dbf82f2ff462a2ba26930
Armaritto <armia.joseph35@gmail.com>
2025-07-23 20:36:21 +0300

    work done: add needed dtos

---
+++ Dtos/SignInRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Skill_Hub.Dtos
+{
+    public class SignInRequestDTO
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
