fe66872950bdb642d118df223630fb6509d24971
Armaritto <armia.joseph35@gmail.com>
2025-07-23 15:25:04 +0300

    work done: 1. add instructor repo interface 2. add instructor repo implementation

---
+++ Repositories/Implementations/InstructorRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skill_Hub.Data;
+using Skill_Hub.Models;
+using Skill_Hub.Repositories.Interfaces;
+
+namespace Skill_Hub.Repositories.Implementations
+{
+    public class InstructorRepository : IInstructorRepository
+    {
+        private readonly Context _context;
+
+        public InstructorRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Instructor>> GetAllInstructorsAsync()
+        {
+            return await _context.Instructors.ToListAsync();
+        }
+
+        public async Task<Instructor?> GetInstructorByIdAsync(int id)
+        {
+            return await _context.Instructors.FindAsync(id);
+        }
+    }
+}
