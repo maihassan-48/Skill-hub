e7a208173b1f6626f44aaf2049b9cc1dee70c218
Armaritto <armia.joseph35@gmail.com>
2025-07-23 15:32:15 +0300

    work done: 1. add instructor service interface 2. add instructor service implementation

---
+++ Services/Implementations/InstructorService.cs
@@ -0,0 +1,25 @@
+﻿using Skill_Hub.Models;
+using Skill_Hub.Services.Interfaces;
+
+namespace Skill_Hub.Services.Implementations
+{
+    public class InstructorService : IInstructorService
+    {
+        private readonly IInstructorService _instructorService;
+
+        public InstructorService(IInstructorService instructorService)
+        {
+            _instructorService = instructorService;
+        }
+
+        public async Task<IEnumerable<Instructor>> GetAllInstructorsAsync()
+        {
+            return await _instructorService.GetAllInstructorsAsync();
+        }
+
+        public async Task<Instructor?> GetInstructorByIdAsync(int id)
+        {
+            return await _instructorService.GetInstructorByIdAsync(id);
+        }
+    }
+}
---
+++ Services/Interfaces/IInstructorService.cs
@@ -0,0 +1,10 @@
+﻿using Skill_Hub.Models;
+
+namespace Skill_Hub.Services.Interfaces
+{
+    public interface IInstructorService
+    {
+        Task<Instructor?> GetInstructorByIdAsync(int id);
+        Task<IEnumerable<Instructor>> GetAllInstructorsAsync();
+    }
+}
