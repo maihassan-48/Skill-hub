3bacca576a8149b535d2d12d06063427ce22434a
Armaritto <armia.joseph35@gmail.com>
2025-07-23 20:37:08 +0300

    work done: add the user service

---
+++ Services/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Skill_Hub.Dtos;
+using Skill_Hub.Models;
+
+namespace Skill_Hub.Services.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<List<UserResponseDTO>> GetAll();
+        public Task<UserResponseDTO?> GetById(int id);
+        public Task<UserResponseDTO?> GetByName(string name);
+        public Task<UserResponseDTO?>  GetByEmail(string email);
+        public Task<SignInResponseDTO> Create(UserRequestDTO user);
+        public Task Update(UserRequestDTO user, int id);
+        public Task Delete(int id);
+        public Task<SignInResponseDTO> SignIn(SignInRequestDTO signInRequestDTO);
+    }
+}
