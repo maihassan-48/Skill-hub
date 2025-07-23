5f92ccb5e8206d43d053c57c921ef84023e4bc8a
Armaritto <armia.joseph35@gmail.com>
2025-07-23 19:53:14 +0300

    work done: implement unit of work design pattern

---
+++ Configurations/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Skill_Hub.Data;
+using Skill_Hub.Repositories.Implementations;
+using Skill_Hub.Repositories.Interfaces;
+
+namespace Skill_Hub.Configurations
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private Context _context;
+        private IInstructorRepository _instructorRepository;
+
+        public UnitOfWork(Context context)
+        {
+            _context = context;
+        }
+
+        public IInstructorRepository instructorRepository
+        {
+            get
+            {
+                return _instructorRepository = _instructorRepository ?? new InstructorRepository(_context);
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
