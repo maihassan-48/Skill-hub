8409c5eba50a13ee77651f2b9d3c7936c2947d91
Armaritto <armia.joseph35@gmail.com>
2025-07-26 20:04:27 +0300

    work done: fix the many to many relationship

--- Models/Enrollment.cs
+++ Models/Enrollment.cs
@@ -7,6 +7,8 @@ namespace Skill_Hub.Models
         public int Id { get; set; }
         public int StudentId { get; set; }
         public int CourseId { get; set; }
+        public Student Student { get; set; }
+        public Course Course { get; set; }
 
         public DateTime EnrollmentDate { get; set; } = DateTime.UtcNow;
         public DateTime? CompletionDate { get; set; }
