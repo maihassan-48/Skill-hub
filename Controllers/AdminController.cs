4d82a4207d8b773207b483d2a5aa1c50e512d03f
Mohamed Khaled <mohamed.khaled1872002@gmail.com>
2025-07-27 18:01:07 +0300

    work done:
    -add students and admins authorizations on edpoints based on matched roles

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -24,7 +24,7 @@ namespace Skill_Hub.Controllers
             _jwtService = jwtService;
         }
 
-        //[Authorize(Roles = ADMIN_ROLE)]
+        [Authorize(Roles = ADMIN_ROLE)]
         [HttpGet]
         public async Task<IActionResult> GetAdmins()
         {
@@ -32,7 +32,7 @@ namespace Skill_Hub.Controllers
             return Ok(admins);
         }
 
-        //[Authorize(Roles = ADMIN_ROLE)]
+        [Authorize(Roles = ADMIN_ROLE)]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetAdmin(int id)
         {
@@ -40,7 +40,7 @@ namespace Skill_Hub.Controllers
             return Ok(admin);
         }
 
-        //[Authorize(Roles = ADMIN_ROLE)]
+        [Authorize(Roles = ADMIN_ROLE)]
         [HttpPost]
         public async Task<IActionResult> CreateAdmin([FromBody] AdminRequestDTO admin)
         {
@@ -49,7 +49,7 @@ namespace Skill_Hub.Controllers
             return Ok(signInResponse);
         }
 
-        //[Authorize(Roles = ADMIN_ROLE)]
+        [Authorize(Roles = ADMIN_ROLE)]
         [HttpPut]
         public async Task<IActionResult> UpdateAdmin(int id, AdminRequestDTO admin)
         {
--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -24,7 +24,7 @@ namespace Skill_Hub.Controllers
             _jwtService = jwtService;
         }
 
-        //[Authorize(Roles = ADMIN_ROLE)]
+        [Authorize(Roles = INSTRUCTOR_ADMIN_ROLES)]
         [HttpGet]
         public async Task<IActionResult> GetStudents()
         {
@@ -32,7 +32,7 @@ namespace Skill_Hub.Controllers
             return Ok(students);
         }
 
-        //[Authorize(Roles = ADMIN_ROLE)]
+        [Authorize(Roles = INSTRUCTOR_ADMIN_ROLES)]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetStudent(int id)
         {
@@ -40,7 +40,7 @@ namespace Skill_Hub.Controllers
             return Ok(student);
         }
 
-        //[Authorize(Roles = ADMIN_ROLE)]
+        [Authorize(Roles = ADMIN_ROLE)]
         [HttpPost]
         public async Task<IActionResult> CreateStudent([FromBody] StudentRequestDTO student)
         {
@@ -49,7 +49,7 @@ namespace Skill_Hub.Controllers
             return Ok(signInResponse);
         }
 
-        //[Authorize(Roles = ADMIN_ROLE)]
+        [Authorize(Roles = ADMIN_ROLE)]
         [HttpPut]
         public async Task<IActionResult> UpdateStudent(int id, StudentRequestDTO student)
         {
